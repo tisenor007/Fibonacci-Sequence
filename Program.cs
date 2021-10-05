1c48f66fd533e008014513c4de1fa9c4a6e30a5e
Trenton Isenor <ronesinotnert@gmail.com>
2021-10-05 15:54:39 -0300

    added recursion method/technique

--- Program.cs
+++ Program.cs
@@ -8,14 +8,19 @@ namespace Fibonacci_Sequence
 {
     class Program
     {
+        public static int[] cache = new int[20];
         static void Main(string[] args)
         {
-            Fpattern();
-            FIB(50);
-            Console.ReadKey(true);
+            
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(FIB(i) + " = " + i);
+            }
+            //Console.ReadKey(true);
         }
 
-        static void FIB(int wantedValue)
+        //gives you any value from enter number in numeral order
+        static void Fib(int wantedValue)
         {
             double firstValue = 0;
             double secondValue = 1;
@@ -33,21 +38,41 @@ namespace Fibonacci_Sequence
             Console.WriteLine(result);
         }
 
+        //fibonacci pattern that catches when it breaks
         static void Fpattern()
         {
-            double firstValue = 0;
-            double secondValue = 1;
-            double result = 0;
-            while (true)
+            int firstValue = 0;
+            int secondValue = 1;
+            int result = 0;
+            int maxNumber = 0;
+            bool stop = false;
+
+            while (stop == false)
             {
                 result = firstValue + secondValue;
+                if (result < maxNumber)
+                {
+                    stop = true;
+                    break;
+                }
+                maxNumber = result;
 
                 Console.WriteLine();
                 Console.WriteLine(result);
-                Console.ReadKey(true);
                 firstValue = secondValue;
                 secondValue = result;
+                
+                
             }
+            Console.ReadKey(true);
+           
+        }
+        static int FIB(int r)
+        {
+            if (r <= 1) { return 0; }
+            else if (r == 2) { return 1; }
+            else { return FIB(r - 1) + FIB(r - 2); }
+
         }
     }
 }
